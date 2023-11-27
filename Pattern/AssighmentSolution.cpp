461cf915068b533d4cf9eab5206f5762f23fe221
shiva kumar <115865333+SHIVAKUMAR-KS@users.noreply.github.com>
2023-11-27 15:24:11 +0530

    Add files via upload

---
+++ Pattern/AssighmentSolution.cpp
@@ -0,0 +1,54 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+      cout<<"4"<<" ";
+    }
+    cout<<endl;
+  }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+      cout<<col*col <<" ";
+    }
+    cout<<endl;
+  }
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+      cout<<col*col <<" ";
+    }
+    cout<<endl;
+  }
+}
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=1;row<=5;row++){
+   
+    for(int col=1;col<=5;col++){ 
+      char name='F'+col-1;
+      cout<<name<<" ";
+    }
+    cout<<endl;
+  }
+}
