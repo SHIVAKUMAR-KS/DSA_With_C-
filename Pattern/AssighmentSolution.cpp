10acc8f9d8c912d7d7f11f5b4e3d0870131b4cfb
shiva kumar <kshiva09283@gmail.com>
2023-11-29 07:34:35 +0530

    these are new one

---
+++ Pattern/AssighmentSolution.cpp
@@ -0,0 +1,55 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=1;row<=3;row++){
+    for(int col=1;col<=3;col++){
+      cout<<"4"<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+      cout<<col*col <<" ";
+    }
+    cout<<endl;
+  }
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+      cout<<col*col <<" ";
+    }
+    cout<<endl;
+  }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=1;row<=5;row++){
+   
+    for(int col=1;col<=5;col++){ 
+      char name='F'+col-1;
+      cout<<name<<" ";
+    }
+    cout<<endl;
+  }
+}
+*/
