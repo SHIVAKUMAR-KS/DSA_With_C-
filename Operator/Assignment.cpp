3f083625569215dffa182aa5137d0eda0e8d3236
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2023-11-30 18:35:31 +0530

    changed

---
+++ Operator/Assignment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//        int a=10;
+//        a+=15;
+//        cout<<a;     
+
+           if(8<5){
+            cout<<"Yes";
+           }else{
+            cout<<"NO";
+           }
+}
